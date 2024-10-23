7c4948118125bec8d65e87eefb005791a3450329
Watt Iamsuri <wiamsuri@gmail.com>
2024-10-23 18:52:28 +0700

    Handle Wrong Protocol (#4)
    * handle wrong protocol
    
    * upgrade version
    
    * build and push latest tag
    
    ---------
    
    Co-authored-by: Watt Iamsuri <watt.iams@kkpfg.com>

--- src/handler/connection.rs
+++ src/handler/connection.rs
@@ -1,6 +1,7 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
+const PROTOCOL_VERSION: char = 'A';
 
 pub struct Connection {
     stream: TcpStream,
@@ -15,37 +16,37 @@ impl Connection {
 
     pub async fn read_frame(&mut self) -> (String, Vec<u8>) {
         let mut header_buffer = [0; 11];
-        let mut payload_buffer = [0; 8];
+        let mut payload_length_buffer = [0; 8];
         let packet_length: u64;
         let message_type: String;
 
         match self.stream.read_exact(&mut header_buffer).await {
             Ok(_) => {
-                assert!(header_buffer[0] as char == 'A', "Protocol error");
-
-                payload_buffer.clone_from_slice(&header_buffer[3..11]);
-                packet_length = u64::from_be_bytes(payload_buffer);
-
-                message_type = match String::from_utf8((&header_buffer[1..3]).to_vec()) {
-                    Ok(mt) => { mt },
-                    Err(_) => { panic!("Invalid string") },
+                payload_length_buffer.clone_from_slice(&header_buffer[3..11]);
+                packet_length = u64::from_be_bytes(payload_length_buffer);
+                if header_buffer[0] as char == PROTOCOL_VERSION {
+                    message_type = match String::from_utf8((&header_buffer[1..3]).to_vec()) {
+                        Ok(mt) => { mt },
+                        Err(_) => { panic!("Invalid string") },
+                    }
+                } else {
+                    message_type = "WP".to_string();
                 }
-            }
+            },
             Err(e) => {
                 tracing::debug!("Failed to read from socket: {}", e);
                 message_type = "CC".to_string();
                 packet_length = 0;
-            }
+            },
         }
 
         let mut payload = vec![0; (packet_length) as usize];
         if packet_length > 0 {
             match self.stream.read_exact(&mut payload).await {
-                Ok(_) => {
-                }
+                Ok(_) => {},
                 Err(e) => {
                     tracing::error!("Failed to read payload: {}", e);
-                }
+                },
             }
         }
         return (message_type, payload);
@@ -58,14 +59,14 @@ impl Connection {
         let mut header_buffer = header.into_bytes();
         header_buffer.extend(payload_length.to_be_bytes());
         match self.stream.write_all(&header_buffer).await {
-            Ok(_) => {}
-            Err(_) => {}
+            Ok(_) => {},
+            Err(_) => {},
         }
 
         if payload_length > 0 {
             match self.stream.write_all(&payload).await {
-                Ok(_) => {}
-                Err(_) => {}
+                Ok(_) => {},
+                Err(_) => {},
             }
         }
     }
